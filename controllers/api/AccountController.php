fd20130d0da4a2bf992bf4acaa13be0758f42b23
fredy.ns <email@fredyns.net>
2017-03-03 22:49:03 +0700

    model list

--- controllers/api/AccountController.php
+++ controllers/api/AccountController.php
@@ -6,24 +6,67 @@ namespace app\controllers\api;
  * This is the class for REST controller "AccountController".
  * empowered with logic base access control
  */
-
 use Yii;
 use yii\filters\AccessControl;
 use yii\helpers\ArrayHelper;
 use app\actioncontrols\AccountActControl;
+use yii\db\Query;
+use app\models\Account;
 
 class AccountController extends \yii\rest\ActiveController
 {
     public $modelClass = 'app\models\Account';
 
-    
     /**
      * @inheritdoc
      */
     public function checkAccess($action, $model = null, $params = [])
     {
         Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
-        
+
         AccountActControl::checkAccess($action, $model, $params);
+    }
+
+    /**
+     * profiding model list
+     *
+     * @param string $q
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionList($q = null, $id = null)
+    {
+        $minimumInputLength = 2;
+        Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+        $out = [
+            'results' => [
+                'id' => '',
+                'text' => '',
+            ],
+        ];
+
+        if (!is_null($q) && strlen($q) >= $minimumInputLength) {
+            $query = new Query;
+
+            $query
+                ->select('id, name AS text')
+                ->from('account')
+                ->where(['like', 'name', $q])
+                ->limit(20);
+
+            $command = $query->createCommand();
+            $data = $command->queryAll();
+            $out['results'] = array_values($data);
+        } elseif ($id > 0) {
+            $model = Account::findOne($id);
+
+            if ($model) {
+                $out['results'] = [
+                    'id' => $id,
+                    'text' => $model->name,
+                ];
+            }
+        }
+        return $out;
     }
 }
--- controllers/api/BudgetItemController.php
+++ controllers/api/BudgetItemController.php
@@ -6,24 +6,67 @@ namespace app\controllers\api;
  * This is the class for REST controller "BudgetItemController".
  * empowered with logic base access control
  */
-
 use Yii;
 use yii\filters\AccessControl;
 use yii\helpers\ArrayHelper;
 use app\actioncontrols\BudgetItemActControl;
+use yii\db\Query;
+use app\models\BudgetItem;
 
 class BudgetItemController extends \yii\rest\ActiveController
 {
     public $modelClass = 'app\models\BudgetItem';
 
-    
     /**
      * @inheritdoc
      */
     public function checkAccess($action, $model = null, $params = [])
     {
         Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
-        
+
         BudgetItemActControl::checkAccess($action, $model, $params);
+    }
+
+    /**
+     * profiding model list
+     *
+     * @param string $q
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionList($q = null, $id = null)
+    {
+        $minimumInputLength = 2;
+        Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+        $out = [
+            'results' => [
+                'id' => '',
+                'text' => '',
+            ],
+        ];
+
+        if (!is_null($q) && strlen($q) >= $minimumInputLength) {
+            $query = new Query;
+
+            $query
+                ->select('id, code AS text')
+                ->from('budgetItem')
+                ->where(['like', 'code', $q])
+                ->limit(20);
+
+            $command = $query->createCommand();
+            $data = $command->queryAll();
+            $out['results'] = array_values($data);
+        } elseif ($id > 0) {
+            $model = BudgetItem::findOne($id);
+
+            if ($model) {
+                $out['results'] = [
+                    'id' => $id,
+                    'text' => $model->name,
+                ];
+            }
+        }
+        return $out;
     }
 }
